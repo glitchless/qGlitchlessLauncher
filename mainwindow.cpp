c113c978bd002b9ce689abe7cd90a937337d794c
Nikita Kulikov <nikita@kulikof.ru>
2021-02-13 21:42:40 +0200

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->setupUi(this);
+    QPixmap bkgnd( ":/internal/img/background.png");
+    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Background, bkgnd);
+    this->setPalette(palette);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
